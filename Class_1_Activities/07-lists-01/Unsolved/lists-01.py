962b9d3c910cf81ba70156efc10f4c2f2a31a0f9
scoopcash <114896185+scoopcash@users.noreply.github.com>
2022-10-18 21:00:14 -0400

    done with 07

--- Class_1_Activities/07-lists-01/Unsolved/lists-01.py
+++ Class_1_Activities/07-lists-01/Unsolved/lists-01.py
@@ -1,23 +1,23 @@
 # Create a list, `list_1`,  with `0`, `1`, `2`, `3` as values.
-
+List_1 = [0,1,2,3]
 
 # Create a list, `list_2` with `4`, `5`, `6`, `7` as values.
-
+List_2 = [4,5,6,7]
 
 # Create a list, `list_3` with `8`, `9`, `10`, `11` as values.
-
+List_3 = [8,9,10,11]
 
 # Create a list, `list_4` with `12`, `13`, `14`, `15` as values.
-
+List_4 = [12, 13,14,15]
 
 # Print the 3rd index of `list_1`.
-
+print (List_1[2])
 
 # Print the 1st index of `list_2`.
-
+print (List_2[0])
 
 # Print the 2nd index of `list_3`.
-
+print(List_3[1])
 
 # Print the 4th index of `list_4`.
-
+print(List_4[3])
